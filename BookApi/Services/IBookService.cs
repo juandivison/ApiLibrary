2dc8b0d0090c5832efe431309d17712d774756dc
Juan Divison <nosivid@hotmail.com>
2023-07-19 15:05:38 -0400

    Add project files.

---
+++ BookApi/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using BookApi.Model;
+
+namespace BookApi.Services
+{
+    public interface IBookService
+    {
+        Book FindBookById(int id);
+        List<Book> FindAllBooks();
+        void CreateBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(int id);
+    }
+}
---
+++ BookApi/Services/Service.cs
@@ -0,0 +1,40 @@
+﻿using BookApi.Model;
+using System.Data.Entity;
+
+namespace BookApi.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> _repository;
+
+        public BookService(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public Book FindBookById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public List<Book> FindAllBooks()
+        {
+            return (List<Book>)_repository.GetAll();
+        }
+
+        public void CreateBook(Book book)
+        {
+            _repository.Add(book);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _repository.Update(book);
+        }
+
+        public void DeleteBook(int id)
+        {
+            _repository.Delete(_repository.GetById(id));
+        }
+    }
+}
